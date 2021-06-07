8140f4155532d41ebd4e504d895ac3e240daf0a2
davidwpacker <davidwgpacker@gmail.com>
2021-06-07 15:57:06 +1000

    carousel finished. New features to be added.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-/*
-    Grabbing all carousel items and attaching button event listeners
-    
 
-    4) Add event listeners to both buttons 'carousel-button-next' and 'carousel-button-prev' - seeing as though we expect the user to click to change slide position, make sure we set the eventListener to 'click' and the second arguments will be functions!
-    5) Create two functions: moveToNextSlide and moveToPrevSlide - use both of these in action 4 as the second argument on your 'addEventListener' - don't worry about these functions being empty, we will add to them in the upcoming screencasts
-    6) Use console.log to validate that you have 3 'carousel-item' in your 'slides' const
-*/
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
 
-const slides = document.getElementsByClassName("carousel-items");
-let slidePos = 0;
-const totalSlides = slide.length;
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
 
-
-document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide() {})
-document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide() {})
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
 
 function moveToNextSlide() {
-    console.log('moving to the next slide!'); 
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
+
 function moveToPrevSlide() {
-    console.log('moving to the prev slide!'); 
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides -1;
+    } else {
+        slidePosition--;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
 }
+
+
+
+
+
